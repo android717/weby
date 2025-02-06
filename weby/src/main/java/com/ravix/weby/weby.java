40f812b4983ede682e89ea2ec88fd2b981dbca25
Ravi <ravi@togethring.net>
2025-02-06 15:50:05 +0530

    init

---
+++ weby/src/main/java/com/ravix/weby/weby.java
@@ -0,0 +1,13 @@
+package com.ravix.weby;
+
+/**
+ * Created by Ravix
+ * Date: 06-02-2025
+ * Time: 03:48 pm
+ */
+public class weby {
+
+        public static String getMessage() {
+            return "Hello from weby!";
+        }
+}
